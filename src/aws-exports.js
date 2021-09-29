3b129d2d89cb232a3e1e70766827a82832ee1f8c
ValentinGerhard <valentin@amalien.de>
2021-09-29 13:07:45 +0200

    storage init error

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -28,7 +28,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://vuafu6bbl5f7jcc7hfkzlou52e.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-eahtcstrsjgudjzqzmgr3p4ct4"
+    "aws_appsync_apiKey": "da2-eahtcstrsjgudjzqzmgr3p4ct4",
+    "aws_user_files_s3_bucket": "valimage192606-staging",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
